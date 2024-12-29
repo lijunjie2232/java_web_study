9e58f3b8712d849bda8ca02b1d1bbf3448175696
lijunjie2232 <2533584225@qq.com>
2024-12-29 12:17:12 +0800

    spring mvc

--- backend/spring/hello-spring-mvc1/src/main/java/com/li/hellospringmvc1/bean/Handle06Form.java
+++ backend/spring/hello-spring-mvc1/src/main/java/com/li/hellospringmvc1/bean/Handle06Form.java
@@ -4,10 +4,19 @@ import lombok.Data;
 
 @Data
 public class Handle06Form {
-//    username=123&password=321&sex=male&grade=2&favorite=football&favorite=swimming
+    //    username=123&password=321&sex=male&grade=1&address.province=Kyoto&address.city=a&favorite=football&favorite=swimming
     private String username;
     private String password;
     private String sex;
     private int grade;
+    private Address address;
+    private String street;
+    private String zipCode;
     private String[] favorite;
+}
+
+@Data
+class Address {
+    private String province;
+    private String city;
 }
