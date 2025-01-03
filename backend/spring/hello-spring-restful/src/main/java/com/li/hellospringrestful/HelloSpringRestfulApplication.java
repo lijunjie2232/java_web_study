fbf736c3730960761e7740d71f66c4a108c9815b
lijunjie2232 <2533584225@qq.com>
2025-01-03 19:50:19 +0800

    spring mvc

---
+++ backend/spring/hello-spring-restful/src/main/java/com/li/hellospringrestful/HelloSpringRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.li.hellospringrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringRestfulApplication.class, args);
+    }
+
+}
---
+++ backend/spring/hello-spring-restful/src/test/java/com/li/hellospringrestful/HelloSpringRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.hellospringrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
