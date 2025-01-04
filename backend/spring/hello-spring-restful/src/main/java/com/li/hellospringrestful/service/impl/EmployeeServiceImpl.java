c07ba1ccf7c25a8f945a4a57b6af3d697626cb7e
lijunjie2232 <2533584225@qq.com>
2025-01-04 12:14:38 +0800

    spring restful

--- backend/spring/hello-spring-restful/src/main/java/com/li/hellospringrestful/service/impl/EmployeeServiceImpl.java
+++ backend/spring/hello-spring-restful/src/main/java/com/li/hellospringrestful/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,12 @@
 package com.li.hellospringrestful.service.impl;
 
+import ch.qos.logback.core.util.StringUtil;
 import com.li.hellospringrestful.bean.Employee;
 import com.li.hellospringrestful.dao.EmployeeDao;
 import com.li.hellospringrestful.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -25,7 +27,15 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public void updateEmployee(Employee employee) {
-        employeeDao.updateEmployee(employee);
+        assert employee.getId() != null;
+        Employee employeeOrigin = employeeDao.getEmployeeById(employee.getId());
+        employeeOrigin.setName(StringUtils.hasText(employee.getName()) ? employee.getName() : employeeOrigin.getName());
+        employeeOrigin.setAge(employee.getAge() != null ? employee.getAge() : employeeOrigin.getAge());
+        employeeOrigin.setEmail(StringUtils.hasText(employee.getEmail()) ? employee.getEmail() : employeeOrigin.getEmail());
+        employeeOrigin.setGender(employee.getGender() != null ? employee.getGender() : employeeOrigin.getGender());
+        employeeOrigin.setAddress(StringUtils.hasText(employee.getAddress()) ? employee.getAddress() : employeeOrigin.getAddress());
+        employeeOrigin.setSalary(employee.getSalary() != null ? employee.getSalary() : employeeOrigin.getSalary());
+        employeeDao.updateEmployee(employeeOrigin);
     }
 
     @Override
