dbfa2db71cc7a0cc82504a643b1ea722e4c00c81
lijunjie2232 <2533584225@qq.com>
2025-01-04 12:08:32 +0800

    spring restful

--- backend/spring/hello-spring-restful/src/main/java/com/li/hellospringrestful/service/EmployeeService.java
+++ backend/spring/hello-spring-restful/src/main/java/com/li/hellospringrestful/service/EmployeeService.java
@@ -1,4 +1,28 @@
 package com.li.hellospringrestful.service;
 
+import com.li.hellospringrestful.bean.Employee;
+
 public interface EmployeeService {
+
+    /*
+    for sql table:
+    CREATE TABLE IF NOT EXISTS `employee`
+    (
+        `id`      INT(11)        NOT NULL AUTO_INCREMENT,
+        `name`    VARCHAR(255)   NOT NULL,
+        `age`     INT(11)        NOT NULL,
+        `email`   VARCHAR(255)   NOT NULL,
+        `gender`  INT1           NOT NULL,
+        `address` VARCHAR(255)   NOT NULL,
+        `salary`  DECIMAL(10, 2) NOT NULL,
+        PRIMARY KEY (`id`) USING BTREE
+    );
+     */
+    void addEmployee(Employee employee);
+
+    void deleteEmployee(int id);
+
+    void updateEmployee(Employee employee);
+
+    Employee getEmployee(int id);
 }
--- backend/spring/hello-spring-restful/src/main/java/com/li/hellospringrestful/service/impl/EmployeeServiceImpl.java
+++ backend/spring/hello-spring-restful/src/main/java/com/li/hellospringrestful/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,35 @@
 package com.li.hellospringrestful.service.impl;
 
+import com.li.hellospringrestful.bean.Employee;
+import com.li.hellospringrestful.dao.EmployeeDao;
 import com.li.hellospringrestful.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+
+    @Override
+    public void addEmployee(Employee employee) {
+        employeeDao.addEmployee(employee);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeDao.deleteEmployee(id);
+    }
+
+    @Override
+    public void updateEmployee(Employee employee) {
+        employeeDao.updateEmployee(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        return employeeDao.getEmployeeById(id);
+    }
 }
